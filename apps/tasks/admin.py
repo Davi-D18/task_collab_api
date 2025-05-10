349888d0375f08bad9b42fea8332e44e41e35aa7
Davi-D18 <luizdavi.code@gmail.com>
2025-05-10 00:47:08 -0300

    Alterações no mdoels e adição do admin

--- apps/tasks/admin.py
+++ apps/tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from apps.tasks.models.tasks import Tasks
 
-# Register your models here.
+
+@admin.register(Tasks)
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'titulo', 'descricao', 'prioridade', 'prazo', 'status', 'criado_em')
+    list_filter = ('status', 'prioridade', 'usuario',)
+    search_fields = ('titulo', 'descricao')
+    ordering = ('-criado_em',)
+    list_per_page = 20
+    list_editable = ('status',)
--- apps/tasks/models/tasks.py
+++ apps/tasks/models/tasks.py
@@ -1,16 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+PRIORIDADES = [
+    ("B", "Baixa"),
+    ("M", "Media"),
+    ("A", "Alta")
+]
+
+STATUS = [
+    ("P", "Pendente"),
+    ("EA", "Em Andamento"),
+    ("C", "Concluída")
+]
 
 class Tasks(models.Model):
-    titulo = models.CharField(null=True, max_length=160)
-    descricao = models.TextField()
-    prioridade = models.CharField(choices=([("B", "Baixa"), ("M", "Media"), ("A", "Alta")]))
-    prazo = models.DateField()
-    status = models.CharField(choices=[("P", "Pendente"), ("EA", "Em Andamento"), ("C", "Concluída")], default="P")
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Usuário")
+    titulo = models.CharField(null=True, max_length=160, verbose_name="Titulo")
+    descricao = models.TextField(verbose_name="Descrição", blank=True)
+    prioridade = models.CharField(choices=PRIORIDADES, verbose_name="Prioridade")
+    prazo = models.DateField(verbose_name="Prazo", null=True)
+    status = models.CharField(choices=STATUS, default="P", verbose_name="Status", max_length=2)
+    criado_em = models.DateTimeField(auto_now_add=True, verbose_name="Criado em")
+
+    def get_status(self):
+        match self.status:
+            case "P":
+                return "Pendente"
+            case "EA":
+                return "Em Andamento"
+            case "C":
+                return "Concluída"
+            case _:
+                return "Status desconhecido"
+
 
     class Meta:
         ordering = ['id']
-        verbose_name = '' # nome da tabela no banco
+        verbose_name = 'Tarefa' # nome da tabela no banco
+        verbose_name_plural = 'Tarefas'
 
     def __str__(self):
-        return self.titulo # Exibe o campo name
+        return self.titulo # Exibe o campo titulo
