b7bd34ebeb8b244cc22e9c4dbbee2e4519a6bdae
Davi-D18 <luizdavi.code@gmail.com>
2025-05-12 12:24:13 -0300

    removido rota /redoc

--- core/urls.py
+++ core/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from django.utils.translation import gettext_lazy as _
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="task_collab_api API",
+      title=_("task_collab_api API"),
       default_version='v1',
-      description="task_collab_api API documentation",
+      description=_("API para gerenciamento de tarefas colaborativas"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
@@ -21,5 +22,4 @@ urlpatterns = [
    path('api/v1/accounts/', include('apps.accounts.urls')),
    
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
