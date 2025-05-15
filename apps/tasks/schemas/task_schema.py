8ee48cda5ee02e3adebac6ee1d1515de0e020b0b
Davi-D18 <luizdavi.code@gmail.com>
2025-05-15 11:12:09 -0300

    Alterações no serializer do app "tasks"

--- apps/tasks/schemas/task_schema.py
+++ apps/tasks/schemas/task_schema.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from apps.tasks.models.tasks import Tasks, STATUS, PRIORIDADES
+from apps.tasks.models.tasks import Tasks
 from django.contrib.auth.models import User
 
 class TaskSerializer(serializers.ModelSerializer):
@@ -8,6 +8,9 @@ class TaskSerializer(serializers.ModelSerializer):
         slug_field='username',
         write_only=True,
     )
+
+    status = serializers.CharField(write_only=True, required=False)
+    prioridade = serializers.CharField(write_only=True, required=True)
 
     status_display = serializers.CharField(source='get_status_display', read_only=True)
     prioridade_display = serializers.CharField(source='get_prioridade_display', read_only=True)
