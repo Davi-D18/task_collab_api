33a67e97bbf0f4840e43783ded866875a815641f
Davi-D18 <luiz.araujo@alunomicrolins.com.br>
2025-05-20 20:53:29 -0300

    Alteração do banco de produção

--- core/settings/base.py
+++ core/settings/base.py
@@ -121,7 +121,7 @@ DATABASES = {
         'NAME': 'database.db',
     },
     'production': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
